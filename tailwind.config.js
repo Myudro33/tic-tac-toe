0482048c6711cf9cacea6e902f5270720043ed2e
Myudro33 <qanashvili.myudro@gmail.com>
2022-10-09 12:47:50 +0400

    set up vol:2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      'xs': '375px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1440px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
